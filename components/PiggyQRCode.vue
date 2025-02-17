17880f986b493b337930d3287b7d663ec8bbe8e8
thespielplatz <informatics@gmx.net>
2025-02-17 21:31:14 +0100

    fix: add deleted PiggyQRCode Template

---
+++ components/PiggyQRCode.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- eslint-disable vue/no-v-html -->
+  <div v-html="printQrCode" />
+  <!-- eslint-enable vue/no-v-html -->
+</template>
+
+<script setup lang="ts">
+
+import QRCode from 'qrcode-svg'
+
+const printQrCode = ref('')
+
+onMounted(async () => {
+  printQrCode.value = new QRCode({
+    content: window.location.origin,
+    padding: 0,
+    width: 175,
+    height: 175,
+    color: '#1e4563',
+    background: '#f8a394',
+  }).svg()
+})
+
+</script>
